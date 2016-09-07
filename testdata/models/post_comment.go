867b6a40b77f91a0a3b49ef6f7de3b95657a6a88
Daisuke Fujita <dtanshi45@gmail.com>
2016-09-07 15:30:22 +0900

    Modify PostComment testdata

--- testdata/models/post_comment.go
+++ testdata/models/post_comment.go
@@ -2,8 +2,8 @@ package models
 
 import "time"
 
-type PostComments struct {
-	ID        uint       `gorm:"primary_key;AUTO_INCREMENT" json:"id"`
+type PostComment struct {
+	ID        uint       `json:"id"`
 	UserID    uint       `json:"user_id"`
 	User      *User      `json:"user"` // This line is infered from column name "user_id".
 	PostID    uint       `json:"post_id"`

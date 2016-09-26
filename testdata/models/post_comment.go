d6de79bc6e357639f61c74277cb8209fc413fa5a
Dan <dan.fujimura@gmail.com>
2016-09-26 20:02:18 +0900

    Add testdata

--- testdata/models/post_comment.go
+++ testdata/models/post_comment.go
@@ -5,6 +5,7 @@ import "time"
 type PostComment struct {
 	ID        uint       `json:"id"`
 	UserID    uint       `json:"user_id"`
+	User      *User      `json:"user"` // This line is infered from column name "user_id".
 	PostID    uint       `json:"post_id"`
 	Content   string     `json:"content"`
 	CreatedAt *time.Time `json:"created_at"`
